b0855390874ba3dc2e43a0ca3654c07b85af1306
임명섭 <lms@elonsoft.co.kr>
2022-06-08 23:22:33 +0900

    숫자 야구 게임 결과 출력 View 구현

---
+++ src/main/java/baseball/view/ResultView.java
@@ -0,0 +1,15 @@
+package baseball.view;
+
+import baseball.domain.Score;
+
+public class ResultView {
+
+    public static void print(Score score) {
+        System.out.println(score);
+    }
+
+    public static void endGame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+}
