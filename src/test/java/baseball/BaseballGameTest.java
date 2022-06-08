4acb157a111d4469391efcafac0b2b3806a1478e
임명섭 <lms@elonsoft.co.kr>
2022-06-08 23:21:19 +0900

    숫자 야구 게임 결과 계산 테스트 추가

---
+++ src/test/java/baseball/BaseballGameTest.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import baseball.domain.BaseballGame;
+import baseball.domain.Score;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class BaseballGameTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"713:123:1볼:1스트라이크", "467:469:'':2스트라이크"}, delimiter = ':')
+    void 숫자_야구_게임_실행(String answer, String input, String ball, String strike) {
+        BaseballGame baseballGame = new BaseballGame(answer);
+        Score score = baseballGame.execute(input);
+        String expected = ball + " " + strike;
+        assertThat(score.toString()).isEqualTo(expected.trim());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"21","223"})
+    void 숫자_야구_게임_입력값_유효성_검사(String input) {
+        BaseballGame baseballGame = new BaseballGame("123");
+        assertThatThrownBy(() -> baseballGame.execute(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
